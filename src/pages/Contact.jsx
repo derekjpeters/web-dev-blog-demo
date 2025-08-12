7b7d7826e4fdcd8c712d7630e03aca767cb51b48
derekjpeters <derek.j.peters@gmail.com>
2025-08-12 19:46:07 -0400

    WIP contact form

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+export default function Contact() {
+    const navigate = useNavigate();
+    const [formData, setFormData] = useState({ name: '', message: ''});
+
+    //Handle form changes dynamically
+    const handleChange = (e) => {
+        //1. Extract the "name" and "value" from the form element that user inputs behind the scenes this triggers an event
+        const { name, value } = e.target;
+        //2. Update the form state by copying the previous state with ...prev
+        //3. Change only the field that matches the input's "name" attribute
+        setFormData(prev => ({
+            ...prev,  //copy all previous form field and values
+            [name]: value})); //overwrite just the field that is changed by changing the value with the user input
+    }
+
+    //Handle Submit
+    const HandleSubmit = (e) => {
+        //1. Prevent the browser's default form submission behavior
+        e.preventDefault();
+        //2. Log the current form data
+        console.log('Form Submitted', formData); 
+        //3. Reset the form
+        setFormData({ name: '', message: '' });
+        //4. Route the user to the "Thank you" component using React Router's useNavigate();
+        navigate('/thank-you')
+    }
+
+
+
+}
